c7da1b5118188703c3b41e451672972bea354e92
Keremenci <mkmenemencioglu@hotmail.com>
2024-08-20 02:34:09 +0300

    Add IAM Role and credential helper

--- infra/main.tf
+++ infra/main.tf
@@ -79,6 +79,57 @@ resource "aws_key_pair" "sysadmin" {
   public_key = file("./secrets/sysadmin_ed25519.pub")
 }
 
+# IAM role for EC2 to access ECR
+
+resource "aws_iam_role" "ec2_role" {
+  name = "bestiary-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "bestiary-ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "bestiary-ecr-policy"
+  description = "Policy to allow EC2 to pull images from ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ecr_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+
 # EC2 instance that will run the app
 
 resource "aws_instance" "app" {
@@ -128,6 +179,13 @@ resource "aws_instance" "app" {
 
               sudo chmod 600 /home/bestiary/.ssh/authorized_keys
               sudo chown -R bestiary:bestiary /home/bestiary
+
+              # Install credential helper
+              sudo apt-get -qq --yes --force-yes install amazon-ecr-credential-helper
+              sudo mkdir -p /home/ubuntu/.docker
+              sudo mkdir -p /home/bestiary/.docker
+              sudo bash -c 'echo "{\"credsStore\": \"ecr-login\"}" > /home/ubuntu/.docker/config.json'
+              sudo bash -c 'echo "{\"credsStore\": \"ecr-login\"}" > /home/bestiary/.docker/config.json'
               EOF
 }
 
